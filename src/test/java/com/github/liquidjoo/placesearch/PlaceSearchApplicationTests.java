26910ddfc7e12e68de6a11b6562b26f65b3e8146
liquidjoo <liquidjoo@gmail.com>
2020-09-16 19:50:29 +0900

    init : project start

---
+++ src/test/java/com/github/liquidjoo/placesearch/PlaceSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.liquidjoo.placesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
