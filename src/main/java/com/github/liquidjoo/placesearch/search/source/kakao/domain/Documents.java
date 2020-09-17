f49aee72f18a464c0e6b918223c3b48f64fba8be
liquidjoo <liquidjoo@gmail.com>
2020-09-18 02:52:48 +0900

    feat : documents mapping by api response

--- src/main/java/com/github/liquidjoo/placesearch/search/source/kakao/domain/Documents.java
+++ src/main/java/com/github/liquidjoo/placesearch/search/source/kakao/domain/Documents.java
@@ -1,4 +1,24 @@
 package com.github.liquidjoo.placesearch.search.source.kakao.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ToString
 public class Documents {
+    @JsonProperty(value = "documents")
+    private List<Document> documents;
+
+    @JsonProperty(value = "meta")
+    private Meta meta;
+
+    public List<Document> getDocuments() {
+        return new ArrayList<>(documents);
+    }
+
+    public Meta getMeta() {
+        return meta;
+    }
 }
--- src/test/java/com/github/liquidjoo/placesearch/search/search/infra/KakaoAddressSearchSourceTest.java
+++ src/test/java/com/github/liquidjoo/placesearch/search/search/infra/KakaoAddressSearchSourceTest.java
@@ -1,5 +1,6 @@
 package com.github.liquidjoo.placesearch.search.search.infra;
 
+import com.github.liquidjoo.placesearch.search.source.kakao.domain.Documents;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.*;
@@ -36,6 +37,33 @@ class KakaoAddressSearchSourceTest {
                 () -> assertThat(responseEntity.getBody().containsKey("meta")).isTrue(),
                 () -> assertThat(responseEntity.getBody().containsKey("documents")).isTrue()
         );
+    }
 
+    @Test
+    @DisplayName("api response object mapping")
+    void objectMappingByApiResponse() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "KakaoAK 7fbc48e75f1528c602cd183aeca7e2b0");
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://dapi.kakao.com/v2/local/search/address.json?query={query}";
+
+        HttpEntity request = new HttpEntity(headers);
+        final ResponseEntity<Documents> responseEntity = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                request,
+                Documents.class,
+                "전북 삼성동 100"
+        );
+
+        final Documents documents = responseEntity.getBody();
+
+        assertAll("local api response",
+                () -> assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK),
+                () -> assertThat(documents.getDocuments()).isNotNull(),
+                () -> assertThat(documents.getMeta()).isNotNull()
+        );
     }
 }
