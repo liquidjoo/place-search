ed84327c34ef41b87cf846c14018cda244b99380
liquidjoo <liquidjoo@gmail.com>
2020-09-18 02:37:58 +0900

    feat : meta object mapping test

---
+++ src/main/java/com/github/liquidjoo/placesearch/search/source/kakao/domain/Meta.java
@@ -0,0 +1,18 @@
+package com.github.liquidjoo.placesearch.search.source.kakao.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class Meta {
+    @JsonProperty(value = "is_end")
+    private boolean isEnd;
+
+    @JsonProperty(value = "pageable_count")
+    private int pageableCount;
+
+    @JsonProperty(value = "total_count")
+    private int totalCount;
+}
